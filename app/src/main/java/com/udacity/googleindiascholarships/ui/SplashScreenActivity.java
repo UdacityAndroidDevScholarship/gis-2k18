beb2b85b01210ef1ad90d2ffad6c1a5780a8792f
Tavish Jain <31106411+tavishjain@users.noreply.github.com>
2018-04-15 22:11:10 +0530

    (Issue #18) Adds Splash Screen with animations (#19)
    Adds Splash Screen with animations

---
+++ app/src/main/java/com/udacity/googleindiascholarships/ui/SplashScreenActivity.java
@@ -0,0 +1,83 @@
+package com.udacity.googleindiascholarships.ui;
+
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewTreeObserver;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.udacity.googleindiascholarships.R;
+
+/**
+ * Created by tavishjain on 15-04-2018.
+ */
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final Long SPLASH_DELAY = 3000L;
+    private Handler mDelayHandler = new Handler();
+    private TextView mAppNameView;
+    private ImageView mAppLogoView;
+
+    private Runnable runnable = new Runnable() {
+        @Override public void run() {
+            Intent intent = new Intent(SplashScreenActivity.this , MainActivity.class);
+            finish();
+            overridePendingTransition(R.anim.enter, R.anim.exit);
+            startActivity(intent);
+        }
+    };
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        mAppLogoView = findViewById(R.id.iv_app_icon);
+        mAppNameView = findViewById(R.id.tv_app_name);
+
+        mDelayHandler.postDelayed(runnable, SPLASH_DELAY);
+
+        startAnimation();
+    }
+
+    private void startAnimation() {
+        mAppNameView.getViewTreeObserver()
+                .addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override public void onGlobalLayout() {
+                        mAppNameView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                        AnimatorSet mAnimatorSet = new AnimatorSet();
+                        mAnimatorSet.playTogether(
+                            ObjectAnimator.ofFloat(mAppNameView, "alpha", 0, 1, 1, 1),
+                            ObjectAnimator.ofFloat(mAppNameView, "scaleX", 0.3f, 1.05f, 0.9f, 1),
+                            ObjectAnimator.ofFloat(mAppNameView, "scaleY", 0.3f, 1.05f, 0.9f, 1));
+                        mAnimatorSet.setDuration(2000);
+                        mAnimatorSet.start();
+                    }
+                });
+        mAppLogoView.getViewTreeObserver()
+                .addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                    @Override public void onGlobalLayout() {
+                        mAppLogoView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                        AnimatorSet mAnimatorSet = new AnimatorSet();
+                        mAnimatorSet.playTogether(
+                            ObjectAnimator.ofFloat(mAppLogoView, "alpha", 0, 1, 1, 1),
+                            ObjectAnimator.ofFloat(mAppLogoView, "scaleX", 0.3f, 1.05f, 0.9f, 1),
+                            ObjectAnimator.ofFloat(mAppLogoView, "scaleY", 0.3f, 1.05f, 0.9f, 1));
+                        mAnimatorSet.setDuration(2000);
+                        mAnimatorSet.start();
+                    }
+                });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mDelayHandler.removeCallbacks(runnable);
+    }
+}
