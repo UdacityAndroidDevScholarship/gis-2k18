228f18a466b272ec702b1932d230c2949781389c
RAHUL YADAV <ryadav531996@gmail.com>
2018-05-05 03:25:50 +0530

    Local Database Basic Structure.
    1.Added Contract class [ProjectContract.java].
    2.Added DbHelper class [ProjectDbHelper.java].

--- app/src/main/java/com/udacity/googleindiascholarships/projects/db/ProjectContract.java
+++ app/src/main/java/com/udacity/googleindiascholarships/projects/db/ProjectContract.java
@@ -1,4 +1,39 @@
 package com.udacity.googleindiascholarships.projects.db;
 
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
 public class ProjectContract {
+
+    public static abstract class ProjectEntry implements BaseColumns {
+        public static final String TABLE_NAME = "projectGIS";
+        public static final String _ID = BaseColumns._ID;
+        public static final String COLUMN_PROJECT_NAME = "project_name";
+        public static final String COLUMN_PROJECT_DESCRIPTION = "project_description";
+        public static final String COLUMN_PROJECT_LOGO_URL = "project_logo_url";
+        public static final String COLUMN_PROJECT_GITHUB_URL = "project_github_url";
+
+
+        //Define the string which contains package name//
+        public static final String CONTENT_AUTHORITY = "com.udacity.googleindiascholarships";
+        public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+        public static final String PATH_PROJECTS = "project";
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PROJECTS);
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a list of pets.
+         */
+        public static final String CONTENT_LIST_TYPE =
+                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PROJECTS;
+
+        /**
+         * The MIME type of the {@link #CONTENT_URI} for a single pet.
+         */
+        public static final String CONTENT_ITEM_TYPE =
+                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_PROJECTS;
+
+
+    }
+
 }
--- app/src/main/java/com/udacity/googleindiascholarships/projects/db/ProjectDbHelper.java
+++ app/src/main/java/com/udacity/googleindiascholarships/projects/db/ProjectDbHelper.java
@@ -1,4 +1,34 @@
 package com.udacity.googleindiascholarships.projects.db;
 
-public class ProjectDbHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class ProjectDbHelper extends SQLiteOpenHelper {
+
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "gis2k18.db";
+
+    public ProjectDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String SQL_CREATE_PROJECTS_TABLE = "CREATE TABLE "+ ProjectContract.ProjectEntry.TABLE_NAME+" ("
+                +ProjectContract.ProjectEntry._ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
+                +ProjectContract.ProjectEntry.COLUMN_PROJECT_NAME+" TEXT NOT NULL,"
+                +ProjectContract.ProjectEntry.COLUMN_PROJECT_DESCRIPTION+" TEXT NOT NULL,"
+                +ProjectContract.ProjectEntry.COLUMN_PROJECT_GITHUB_URL+" TEXT NOT NULL,"
+                + ProjectContract.ProjectEntry.COLUMN_PROJECT_LOGO_URL+" TEXT NOT NULL);";
+
+
+        sqLiteDatabase.execSQL(SQL_CREATE_PROJECTS_TABLE);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
