3b2e08ba4b3551dac21e993a03c34e13ec0c2f12
RAHUL YADAV <ryadav531996@gmail.com>
2018-05-10 03:48:35 +0530

    Contact Project Section Firebase (#94)
    Contact Project Section Firebase

--- app/src/main/java/com/udacity/googleindiascholarships/projects/ui/adapter/ContactAdapter.java
+++ app/src/main/java/com/udacity/googleindiascholarships/projects/ui/adapter/ContactAdapter.java
@@ -1,7 +1,10 @@
 package com.udacity.googleindiascholarships.projects.ui.adapter;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +18,8 @@ import com.udacity.googleindiascholarships.projects.entities.ContactModerator;
 import com.udacity.googleindiascholarships.projects.entities.Project;
 import com.udacity.googleindiascholarships.projects.ui.ProjectDetails;
 
+import org.w3c.dom.Text;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +30,13 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactC
     public Context mContext;
     public List<ContactModerator> mListItems;
     public ArrayList<ContactModerator> mProjectListItems;
+    public String profileLink;
+    public String nameContact;
+    public String titleContact;
+    public String linkedInContact;
+    public String googlePlusContact;
+    public String githubContact;
+
 
     public ContactAdapter(Context context, List<ContactModerator> mListItems) {
         mContext = context;
@@ -57,10 +69,97 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ContactC
       CircleImageView contactImageView;
 
 
-        public ContactCardViewHolder(View itemView) {
+        public ContactCardViewHolder(final View itemView) {
             super(itemView);
-
             contactImageView = itemView.findViewById(R.id.civ_contactImageView);
+
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                     int position = getAdapterPosition();
+
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                    builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.cancel();
+                        }
+                    });
+                    View itemView = LayoutInflater.from(mContext).inflate(R.layout.custom_contact,null);
+                    CircleImageView profileImage = itemView.findViewById(R.id.contact_profile);
+                    TextView contact_name = itemView.findViewById(R.id.contact_name);
+                    TextView contact_title = itemView.findViewById(R.id.contact_title);
+                    CircleImageView linkedInButton = itemView.findViewById(R.id.contact_linkedIn);
+                    CircleImageView googlePlus = itemView.findViewById(R.id.contact_googleplus);
+                    CircleImageView githubLink = itemView.findViewById(R.id.contact_github);
+                    TextView emailGis = itemView.findViewById(R.id.gisMailId);
+
+                    Picasso.with(mContext).load(mListItems.get(position).getMod_profile()).into(profileImage);
+                    contact_name.setText(mListItems.get(position).getMod_name());
+                    contact_title.setText(mListItems.get(position).getMod_title());
+
+                    linkedInButton.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            Uri githHubUri = Uri.parse(mListItems.get(getAdapterPosition()).getMod_linkedin());
+                            Intent websiteIntent = new Intent(Intent.ACTION_VIEW, githHubUri);
+                            mContext.startActivity(websiteIntent);
+
+                        }
+                    });
+
+                    googlePlus.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String address [] = {mListItems.get(getAdapterPosition()).getMod_email()};
+                            Intent intent = new Intent(Intent.ACTION_SENDTO);
+                            intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                            intent.putExtra(Intent.EXTRA_EMAIL, address);
+                            intent.putExtra(Intent.EXTRA_SUBJECT, "Query :");
+                            if (intent.resolveActivity(mContext.getPackageManager()) != null) {
+                                mContext.startActivity(intent);
+                            }
+
+                        }
+                    });
+                    githubLink.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            Uri githHubUri = Uri.parse(mListItems.get(getAdapterPosition()).getMod_github());
+                            Intent websiteIntent = new Intent(Intent.ACTION_VIEW, githHubUri);
+                            mContext.startActivity(websiteIntent);
+
+                        }
+                    });
+
+                    emailGis.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+
+                            String address [] = {"gisudacity2k18@gmail.com"};
+                            Intent intent = new Intent(Intent.ACTION_SENDTO);
+                            intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+                            intent.putExtra(Intent.EXTRA_EMAIL, address);
+                            intent.putExtra(Intent.EXTRA_SUBJECT, "Query :");
+                            if (intent.resolveActivity(mContext.getPackageManager()) != null) {
+                                mContext.startActivity(intent);
+                            }
+
+                        }
+                    });
+
+                    builder.setView(itemView);
+
+                    AlertDialog dialog = builder.create();
+                    dialog.setTitle("About");
+                    dialog.show();
+                }
+            });
 
 
 
