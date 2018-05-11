7f6848151e23758e1957459b6184362258a836e6
Sudhanshu Vohra <sudhanshuvohra8443@gmail.com>
2018-05-11 11:07:06 +0530

    Improved the layout of the Home Screen with features like sharing blogs, bookmarking them and un-bookmarking them and opening them in browser (#98)

--- app/src/main/java/com/udacity/googleindiascholarships/ui/adapters/BlogsRecyclerViewAdapter.java
+++ app/src/main/java/com/udacity/googleindiascholarships/ui/adapters/BlogsRecyclerViewAdapter.java
@@ -1,10 +1,16 @@
 package com.udacity.googleindiascholarships.ui.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.squareup.picasso.Picasso;
 import com.udacity.googleindiascholarships.R;
 
 /**
@@ -14,6 +20,11 @@ import com.udacity.googleindiascholarships.R;
 public class BlogsRecyclerViewAdapter extends RecyclerView.Adapter<BlogsRecyclerViewAdapter.BlogsViewHolder> {
 
     Context mContext;
+    // Note: This is just a dummy variable
+    String blogImageURL = "https://cdn-images-1.medium.com/max/1000/1*iFjXqXIxICNAwP9-6LHygQ.jpeg";
+    String blogURL = "https://medium.com/@sudhanshuvohra8443/google-india-challenge-scholarship-nearing-the-end-of-a-great-journey-9216466d2614";
+    private final static String BOOKMARKED = "Bookmarked";
+    private final static String UNBOOKMARKED = "Un-bookmarked";
 
     public BlogsRecyclerViewAdapter(Context context){
         mContext = context;
@@ -28,8 +39,40 @@ public class BlogsRecyclerViewAdapter extends RecyclerView.Adapter<BlogsRecycler
     }
 
     @Override
-    public void onBindViewHolder(BlogsViewHolder holder, int position) {
-
+    public void onBindViewHolder(final BlogsViewHolder holder, int position) {
+        Picasso.with(mContext).load(blogImageURL).into(holder.thumbnailImage);
+        holder.share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent shareArticle = new Intent(Intent.ACTION_SEND);
+                shareArticle.setType("text/plain");
+                shareArticle.putExtra(Intent.EXTRA_TITLE, "Hey I just read this awesome article. Have a look.");
+                shareArticle.putExtra(Intent.EXTRA_TEXT, blogURL);
+                mContext.startActivity(Intent.createChooser(shareArticle, "Share Article"));
+            }
+        });
+        holder.openInBrowser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(blogURL));
+                mContext.startActivity(browserIntent);
+            }
+        });
+        holder.bookmark.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(holder.bookmark.getTag().equals(UNBOOKMARKED)){
+                    holder.bookmark.setTag(BOOKMARKED);
+                    holder.bookmark.setImageResource(R.drawable.ic_bookmark_checked);
+                    Toast.makeText(mContext, "Blog " + BOOKMARKED, Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    holder.bookmark.setTag(UNBOOKMARKED);
+                    holder.bookmark.setImageResource(R.drawable.ic_bookmark);
+                    Toast.makeText(mContext, "Blog " + UNBOOKMARKED, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -38,8 +81,18 @@ public class BlogsRecyclerViewAdapter extends RecyclerView.Adapter<BlogsRecycler
     }
 
     public class BlogsViewHolder extends RecyclerView.ViewHolder {
+
+        ImageButton bookmark, share,openInBrowser;
+        ImageView thumbnailImage;
+
         public BlogsViewHolder(View itemView) {
             super(itemView);
+
+            bookmark = (ImageButton) itemView.findViewById(R.id.ib_bookmark_blog);
+            share = (ImageButton) itemView.findViewById(R.id.ib_share_blog);
+            openInBrowser = (ImageButton) itemView.findViewById(R.id.ib_open_blog);
+            thumbnailImage = (ImageView) itemView.findViewById(R.id.img_view_blog_thumbnail);
+            bookmark.setTag(UNBOOKMARKED);
         }
     }
 }
